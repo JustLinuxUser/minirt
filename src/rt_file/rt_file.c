22a8125fc17be7c8b83df6bbcc58367f5f5701d7
Andrii Dokhniak <adokhniak@gmail.com>
2025-11-01 12:41:45 +0100

    norm

--- src/rt_file/rt_file.c
+++ src/rt_file/rt_file.c
@@ -1,23 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rt_file.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anddokhn <anddokhn@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/01 12:37:47 by anddokhn          #+#    #+#             */
+/*   Updated: 2025/11/01 12:37:47 by anddokhn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rt_consumer.h"
+#include <stddef.h>
 
-bool process_file(char* fpath, t_state *state)
+bool	process_file(char *fpath, t_state *state)
 {
-    t_dyn_str file;
-	bool ret = true;
+	t_dyn_str	file;
+	bool		ret;
+	t_rt_parser	parser;
+	size_t		i;
 
-    dyn_str_init(&file);
-    if (!dyn_str_read_file(fpath, &file))
+	ret = true;
+	dyn_str_init(&file);
+	if (!dyn_str_read_file(fpath, &file))
 		return (false);
-	t_rt_parser parser;
-    parser = (t_rt_parser){.tokenizer.file.contents = file, .tokenizer.file.fname = fpath};
+	parser = (t_rt_parser){.tokenizer.file.contents = file,
+		.tokenizer.file.fname = fpath};
 	if (!parse_file(&parser))
 		ret = false;
 	if (ret && !consume_parsed_nodes(parser, state))
 		ret = false;
-	for (size_t i = 0; i < parser.statements.len; i++) {
+	i = 0;
+	while (i < parser.statements.len)
+	{
 		free_node(parser.statements.buff[i].v);
+		i++;
 	}
 	free(parser.statements.buff);
-    free(file.buff);
-    return (ret);
+	free(file.buff);
+	return (ret);
 }
