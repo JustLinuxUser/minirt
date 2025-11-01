8452c617a61a1e4e4789fe240e8873504899790e
Andrii Dokhniak <adokhniak@gmail.com>
2025-11-01 12:44:02 +0100

    norm

--- src/rt_file/rt_consumer.h
+++ src/rt_file/rt_consumer.h
@@ -1,35 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rt_consumer.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anddokhn <anddokhn@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/01 12:41:54 by anddokhn          #+#    #+#             */
+/*   Updated: 2025/11/01 12:41:54 by anddokhn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef RT_CONSUMER_H
-#define RT_CONSUMER_H
+# define RT_CONSUMER_H
 
-#include "rt_parser.h"
-#include <stdbool.h>
+# include "rt_parser.h"
+# include <stdbool.h>
 
-typedef struct t_rt_consumer {
-	t_rt_parser parser;
+typedef struct t_rt_consumer
+{
+	t_rt_parser			parser;
 
-	bool got_cam;
-	bool got_area;
+	bool				got_cam;
+	bool				got_area;
 
-	int	last_idx;
+	int					last_idx;
 
 	// error info
 	enum RT_ERR_TYPE	err;
-	t_rt_node last_node;
-	t_rt_token last_key;
+	t_rt_node			last_node;
+	t_rt_token			last_key;
 
-	enum RT_NODE_TYPE expected;
-	float range_start;
-	float range_end;
+	enum RT_NODE_TYPE	expected;
+	float				range_start;
+	float				range_end;
 
-	char *last_ident;
-} t_rt_consumer;
+	char				*last_ident;
+}	t_rt_consumer;
 
-typedef struct t_rt_consumer_tl {
-    t_rt_kv* kv;
-    t_rt_consumer* consumer;
-    t_state* state;
-    int i;
-} t_rt_consumer_tl;
-void print_consumer_err(t_rt_consumer* consumer);
-bool process_file(char* fpath, t_state *state);
+typedef struct t_rt_consumer_tl
+{
+	t_rt_kv			*kv;
+	t_rt_consumer	*consumer;
+	t_state			*state;
+	int				i;
+}	t_rt_consumer_tl;
+void	print_consumer_err(t_rt_consumer *consumer);
+bool	process_file(char *fpath, t_state *state);
 #endif
