3a304367ed553bd557a002db3b18fd02e1a427f4
Andrii Dokhniak <adokhniak@gmail.com>
2025-11-01 16:13:38 +0100

    nomr

--- src/tinyobj.c
+++ src/tinyobj.c
@@ -10,50 +10,84 @@
 #include "libft/ft_printf/ft_printf.h"
 #include "obj_loader.h"
 
+
+void	mesh_forward_z(t_mesh *mesh) {
+	t_fvec3	vert;
+	t_fvec3	tmp;
+	size_t	i;
+
+	i = 0;
+	while (i < mesh->vertices.len)
+	{
+		vert = mesh->vertices.buff[i];
+		tmp = vert;
+		tmp.z = vert.y;
+		tmp.y = vert.z;
+		mesh->vertices.buff[i] = tmp;
+		i++;
+	}
+}
+
+t_fvec3	mesh_avg(t_mesh *mesh)
+{
+	t_fvec3	sum;
+	size_t	i;
+
+	i = 0;
+	while (i < mesh->vertices.len)
+	{
+		sum = fvec3_add(mesh->vertices.buff[i], sum);
+		i++;
+	}
+	return (fvec3_scale(sum, 1. / mesh->vertices.len));
+}
+
+void	mesh_transform(t_mesh *mesh, t_obj_spec s)
+{
+	t_fvec3	avg;
+	size_t	i;
+	t_fvec3	vert;
+
+	if (s.forward_z)
+		mesh_forward_z(mesh);
+	avg = mesh_avg(mesh);
+	i = 0;
+	while (i < mesh->vertices.len)
+	{
+		vert = mesh->vertices.buff[i];
+		vert = fvec3_sub(vert, avg);
+		vert = vec3_rotate_yaw_pitch(vert, s.rotation.x, s.rotation.y);
+		vert = fvec3_scale(vert, s.scale);
+		vert = fvec3_add(vert, s.pos);
+		mesh->vertices.buff[i] = vert;
+		i++;
+	}
+}
+
 bool	load_triangles(t_state *state, t_obj_spec s)
 {
 	t_mesh			mesh;
 	t_obj_parser	parser;
+	size_t			i;
+	t_triangle		t;
 
 	mesh = (t_mesh){.spectrum_idx = s.spectrum_idx};
-	if (!parse_obj(s.path, &parser)) {
+	if (!parse_obj(s.path, &parser))
+	{
 		ft_eprintf("Failed to parse the obj file: %s\n", s.path);
 		return (false);
 	}
 	mesh.vertex_idxs = parser.faces;
 	mesh.vertices = parser.vertices;
-	t_fvec3 sum = {0};
-	for (size_t i = 0; i < mesh.vertices.len; i++) {
-		t_fvec3 vert;
-		vert = mesh.vertices.buff[i];
-		t_fvec3 tmp;
-		tmp = vert;
-		if (s.forward_z) {
-			tmp.z = vert.y;
-			tmp.y = vert.z;
-		}
-		mesh.vertices.buff[i] = tmp;
-		vert = tmp;
-		sum = fvec3_add(vert, sum);
-	}
-	t_fvec3 avg = fvec3_scale(sum, 1. / mesh.vertices.len);
-
-	for (size_t i = 0; i < mesh.vertex_idxs.len / 3; i++) {
-		t_triangle t;
+	mesh_transform(&mesh, s);
+	i = 0;
+	while (i < mesh.vertex_idxs.len / 3)
+	{
 		t.mesh_idx = state->meshes.len;
 		t.triangle_idx = i;
 		vec_triangle_push(&state->triangles, t);
-	}
-
-	for (size_t i = 0; i < mesh.vertices.len; i++) {
-		t_fvec3 vert;
-		vert = mesh.vertices.buff[i];
-		vert = fvec3_sub(vert, avg);
-		vert = vec3_rotate_yaw_pitch(vert, s.rotation.x, s.rotation.y);
-		vert = fvec3_scale(vert, s.scale);
-		vert = fvec3_add(vert, s.pos);
-		mesh.vertices.buff[i] = vert;
+		i++;
 	}
 	vec_mesh_push(&state->meshes, mesh);
-	return true;
+	return (true);
 }
