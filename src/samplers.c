9643c926fccb5cdb26a4e326bae9b8c647db127c
mhvduque <mhvduque@gmail.com>
2025-11-02 20:44:55 +0100

    samplers norm

--- src/samplers.c
+++ src/samplers.c
@@ -1,45 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   samplers.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhornero <mhornero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/02 20:37:14 by mhornero          #+#    #+#             */
+/*   Updated: 2025/11/02 20:44:44 by mhornero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "samplers.h"
 #include "minirt.h"
-
-#include <float.h>
+#include "float.h"
 #include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
 
-t_fvec3 rand_direction(uint64_t *rand_state)
+t_fvec3	rand_direction(uint64_t *rand_state)
 {
-    t_fvec3 ret;
+	t_fvec3	ret;
+	float	lensq;
 
-    while (1) {
-        ret.x = rand_float(rand_state) * 2. - 1.;
-        ret.y = rand_float(rand_state) * 2. - 1.;
-        ret.z = rand_float(rand_state) * 2. - 1.;
-        float lensq = fvec3_len_sq(ret);
-        if (lensq > FLT_EPSILON && lensq < 1)
-            break;
-    }
-    return (fvec3_norm(ret));
+	while (1)
+	{
+		ret.x = rand_float(rand_state) * 2. - 1.;
+		ret.y = rand_float(rand_state) * 2. - 1.;
+		ret.z = rand_float(rand_state) * 2. - 1.;
+		lensq = fvec3_len_sq(ret);
+		if (lensq > MACHINE_EPSILON && lensq < 1)
+			break ;
+	}
+	return (fvec3_norm(ret));
 }
 
-t_fvec3 rand_halfsphere(t_fvec3 norm, uint64_t *rand_state) {
-    t_fvec3 ret = rand_direction(rand_state);
+t_fvec3	rand_halfsphere(t_fvec3 norm, uint64_t *rand_state)
+{
+	t_fvec3	ret;
 
-    if (fvec3_dot(ret, norm) < 0) {
-        ret = fvec3_invert(ret);
-    }
-    return (ret);
+	ret = rand_direction(rand_state);
+	if (fvec3_dot(ret, norm) < 0)
+		ret = fvec3_invert(ret);
+	return (ret);
 }
 
-bool sample_stratified(t_sampler_state* state, t_fvec2* sample) {
-    sample->x = (float)state->last_x / state->stratified_x;
-    sample->y = (float)state->last_y / state->stratified_y;
-    state->last_x++;
-    if (state->last_x == state->stratified_x) {
-        state->last_x = 0;
-        state->last_y++;
-    }
-    if (state->last_y > state->stratified_y) {
-        return false;
-    }
-    return true;
+bool	sample_stratified(t_sampler_state *state, t_fvec2 *sample)
+{
+	sample->x = (float)state->last_x / state->stratified_x;
+	sample->y = (float)state->last_y / state->stratified_y;
+	state->last_x++;
+	if (state->last_x == state->stratified_x)
+	{
+		state->last_x = 0;
+		state->last_y++;
+	}
+	if (state->last_y > state->stratified_y)
+		return (false);
+	return (true);
 }
--- src/samplers.h
+++ src/samplers.h
@@ -1,15 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   samplers.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhornero <mhornero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/02 20:35:46 by mhornero          #+#    #+#             */
+/*   Updated: 2025/11/02 20:36:33 by mhornero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include "mymath.h"
 #include <stdbool.h>
 #include <stdint.h>
-typedef struct sampler_state {
-    int stratified_x;
-    int stratified_y;
 
-    int last_x;
-    int last_y;
-} t_sampler_state;
-bool sample_stratified(t_sampler_state* state, t_fvec2* sample);
-t_fvec3 rand_halfsphere(t_fvec3 norm, uint64_t *rand_state);
-t_fvec3 rand_direction(uint64_t *rand_state);
+typedef struct sampler_state
+{
+	int	stratified_x;
+	int	stratified_y;
+	int	last_x;
+	int	last_y;
+}	t_sampler_state;
+
+bool	sample_stratified(t_sampler_state *state, t_fvec2 *sample);
+t_fvec3	rand_halfsphere(t_fvec3 norm, uint64_t *rand_state);
+t_fvec3	rand_direction(uint64_t *rand_state);
