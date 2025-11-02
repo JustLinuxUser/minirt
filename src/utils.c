e37f77340d18cd6b0ffdce0385463d9a64f225c0
mhvduque <mhvduque@gmail.com>
2025-11-02 20:56:40 +0100

    norm utils

--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhornero <mhornero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/02 20:55:24 by mhornero          #+#    #+#             */
+/*   Updated: 2025/11/02 20:55:50 by mhornero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "rt_utils.h"
 
-void free_zero(void *ptr)
+void	free_zero(void *ptr)
 {
-	void **real_ptr = (void **)ptr;
+	void	**real_ptr;
+
+	real_ptr = (void **)ptr;
 	free(*real_ptr);
 	*real_ptr = 0;
 }
