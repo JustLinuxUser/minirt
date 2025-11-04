227d4b707f2bdbd5682eb6e96641b7f3b6ecff67
Andrii Dokhniak <adokhniak@gmail.com>
2025-11-04 14:01:14 +0100

    norm

--- src/mymath2.c
+++ src/mymath2.c
@@ -1,56 +1,58 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   f_vec3_math.c                                      :+:      :+:    :+:   */
+/*   f_vec3.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhornero <mhornero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/02 19:16:57 by mhornero          #+#    #+#             */
-/*   Updated: 2025/11/02 19:17:25 by mhornero         ###   ########.fr       */
+/*   Created: 2025/11/02 19:15:22 by mhornero          #+#    #+#             */
+/*   Updated: 2025/11/02 19:15:56 by mhornero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mymath.h"
+#include <math.h>
 
-t_fvec3	fvec3_add(t_fvec3 a, t_fvec3 b)
+t_fvec3	fvec3_sub(t_fvec3 a, t_fvec3 b)
 {
 	return ((t_fvec3){
-		.x = a.x + b.x,
-		.y = a.y + b.y,
-		.z = a.z + b.z,
+		.x = a.x - b.x,
+		.y = a.y - b.y,
+		.z = a.z - b.z,
 	});
 }
 
-t_fvec3	fvec3_mul(t_fvec3 a, t_fvec3 b)
+float	fvec3_dot(t_fvec3 a, t_fvec3 b)
 {
-	return ((t_fvec3){
-		.x = a.x * b.x,
-		.y = a.y * b.y,
-		.z = a.z * b.z,
-	});
+	return (a.x * b.x + a.y * b.y + a.z * b.z);
 }
 
-t_fvec3	fvec3_scale(t_fvec3 v, float scalar)
+t_fvec3	fvec3_norm(t_fvec3 v)
 {
-	return ((t_fvec3){
-		.x = v.x * scalar,
-		.y = v.y * scalar,
-		.z = v.z * scalar,
-	});
+	t_fvec3	result;
+	float	length;
+	float	ilength;
+
+	result = v;
+	length = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	if (length != 0.0f)
+	{
+		ilength = 1.0f / length;
+		result.x *= ilength;
+		result.y *= ilength;
+		result.z *= ilength;
+	}
+	else
+		result.x = 1;
+	return (result);
 }
 
-t_fvec3	fvec3_reflect(t_fvec3 v, t_fvec3 norm)
+float	fvec3_len_sq(t_fvec3 v)
 {
-	return (fvec3_sub(v,
-			fvec3_scale(norm,
-				2 * fvec3_dot(norm, v))));
+	return (v.x * v.x + v.y * v.y + v.z * v.z);
 }
 
-t_fvec3	fvec3_lerp(t_fvec3 v1, t_fvec3 v2, float prop)
+t_fvec3	fvec3_invert(t_fvec3 v)
 {
-	return ((t_fvec3){
-		.x = v1.x * prop + v2.x * (1 - prop),
-		.y = v1.y * prop + v2.y * (1 - prop),
-		.z = v1.z * prop + v2.z * (1 - prop),
-	});
+	return ((t_fvec3){.x = -v.x, .y = -v.y, .z = -v.z});
 }
