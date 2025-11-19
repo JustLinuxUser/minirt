437c0a67d51fb87c8c933a7c26df4eeb8e142a88
Andrii Dokhniak <anddokhn@c2r8s6.42madrid.com>
2025-11-19 17:34:19 +0100

    norm

--- src/float_utils.h
+++ src/float_utils.h
@@ -6,14 +6,14 @@
 /*   By: anddokhn <anddokhn@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 16:16:19 by anddokhn          #+#    #+#             */
-/*   Updated: 2025/11/05 16:16:19 by anddokhn         ###   ########.fr       */
+/*   Updated: 2025/11/19 17:34:11 by anddokhn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FLOAT_UTILS_H
 # define FLOAT_UTILS_H
 
-# define MACHINE_EPSILON 0x1p-24
+# define MACHINE_EPSILON 0.000000059604644775390625
 # define FLT_EPSILON 1.19209290e-7F
 
 inline static float	f32_gamma(int n)
